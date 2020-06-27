28c6acce9fecd4b28b7657083a2a426b502eae3d
Christian G <Chris@LENOVO>
2020-06-27 13:04:19 -0700

    initial commit

---
+++ src/views/Dashboard.js
@@ -0,0 +1,240 @@
+/*!
+
+=========================================================
+* Paper Dashboard React - v1.2.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/paper-dashboard-react
+* Copyright 2020 Creative Tim (https://www.creative-tim.com)
+
+* Licensed under MIT (https://github.com/creativetimofficial/paper-dashboard-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+// react plugin used to create charts
+import { Line, Pie } from "react-chartjs-2";
+import { default as NumberFormat } from 'react-number-format';
+// reactstrap components
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  CardTitle,
+  Row,
+  Col,
+} from "reactstrap";
+// core components
+import {
+  dashboard24HoursPerformanceChart,
+  dashboardEmailStatisticsChart,
+  dashboardNASDAQChart,
+} from "variables/charts.js";
+
+class Dashboard extends React.Component {
+  state = {
+    loading: true,
+    btcprice: [],
+    usdbtc: []
+  };
+
+  async componentDidMount() {
+    try {
+    const url = "https://localhost:44319/api/coinmarketapi/getprice";
+    const response = await fetch(url);
+    const data = await response.json();
+    this.setState({ btcprice: data.results });
+    }
+    catch(error){
+      console.log(error);
+    }
+    
+    const urlusd = "https://blockchain.info/tobtc?currency=USD&value=1";
+    const responseusd = await fetch(urlusd);
+    const datausd = await responseusd.json();
+    this.setState({ usdbtc: datausd});
+    
+  }
+
+  render() {
+   this.state = {
+     btcprice: {symbol: '', quote:{usd :{price: ''}}}
+   }
+
+    
+       
+    return (
+      <>
+        <div className="content">
+          <Row>
+            <Col lg="3" md="6" sm="6">
+              <Card className="card-stats">
+                <CardBody>
+                 
+                  <Row>
+                    <Col md="4" xs="5">
+                      <div className="icon-big text-center icon-warning">
+                      <img src="https://upload.wikimedia.org/wikipedia/commons/4/46/Bitcoin.svg" />
+                      </div>
+                    </Col>
+                    <Col md="8" xs="7">
+                      <div className="numbers">
+                          <p className="card-category">{this.state.btcprice.symbol}</p>
+                          <NumberFormat value={this.state.btcprice.quote.usd.price} displayType={'text'} decimalScale={2} thousandSeparator={true} prefix={'$'} />
+                        <p />
+                      </div>
+                    </Col>
+                  </Row>
+                </CardBody>
+                <CardFooter>
+                  <hr />
+                  <div className="stats">
+                    <i className="fas fa-sync-alt" /> Update Now
+                  </div>
+                </CardFooter>
+              </Card>
+            </Col>
+            <Col lg="4" md="8" sm="6">
+              <Card className="card-stats">
+                <CardBody>
+                  <Row>
+                    <Col md="4" xs="5">
+                      <div className="icon-big text-center icon-warning">
+                        <i className="nc-icon nc-money-coins text-success" />
+                      </div>
+                    </Col>
+                    <Col md="8" xs="7">
+                      <div className="numbers">
+                        <p className="card-category">USD TO BTC</p>
+                        <CardTitle tag="p">1 USD = {this.state.usdbtc} BTC</CardTitle>
+                        <p />
+                      </div>
+                    </Col>
+                  </Row>
+                </CardBody>
+                <CardFooter>
+                  <hr />
+                  <div className="stats">
+                    <i className="far fa-calendar" /> Last day
+                  </div>
+                </CardFooter>
+              </Card>
+            </Col>
+            <Col lg="3" md="6" sm="6">
+              <Card className="card-stats">
+                <CardBody>
+                  <Row>
+                    <Col md="4" xs="5">
+                      <div className="icon-big text-center icon-warning">
+                        <i className="nc-icon nc-favourite-28 text-primary" />
+                      </div>
+                    </Col>
+                    <Col md="8" xs="7">
+                      <div className="numbers">
+                        <p className="card-category">Followers</p>
+                        <CardTitle tag="p">+45K</CardTitle>
+                        <p />
+                      </div>
+                    </Col>
+                  </Row>
+                </CardBody>
+                <CardFooter>
+                  <hr />
+                  <div className="stats">
+                    <i className="fas fa-sync-alt" /> Update now
+                  </div>
+                </CardFooter>
+              </Card>
+            </Col>
+          </Row>
+          <Row>
+            <Col md="12">
+              <Card>
+                <CardHeader>
+                  <CardTitle tag="h5">Users Behavior</CardTitle>
+                  <p className="card-category">24 Hours performance</p>
+                </CardHeader>
+                <CardBody>
+                  <Line
+                    data={dashboard24HoursPerformanceChart.data}
+                    options={dashboard24HoursPerformanceChart.options}
+                    width={400}
+                    height={100}
+                  />
+                </CardBody>
+                <CardFooter>
+                  <hr />
+                  <div className="stats">
+                    <i className="fa fa-history" /> Updated 3 minutes ago
+                  </div>
+                </CardFooter>
+              </Card>
+            </Col>
+          </Row>
+          <Row>
+            <Col md="4">
+              <Card>
+                <CardHeader>
+                  <CardTitle tag="h5">Email Statistics</CardTitle>
+                  <p className="card-category">Last Campaign Performance</p>
+                </CardHeader>
+                <CardBody>
+                  <Pie
+                    data={dashboardEmailStatisticsChart.data}
+                    options={dashboardEmailStatisticsChart.options}
+                  />
+                </CardBody>
+                <CardFooter>
+                  <div className="legend">
+                    <i className="fa fa-circle text-primary" /> Opened{" "}
+                    <i className="fa fa-circle text-warning" /> Read{" "}
+                    <i className="fa fa-circle text-danger" /> Deleted{" "}
+                    <i className="fa fa-circle text-gray" /> Unopened
+                  </div>
+                  <hr />
+                  <div className="stats">
+                    <i className="fa fa-calendar" /> Number of emails sent
+                  </div>
+                </CardFooter>
+              </Card>
+            </Col>
+            <Col md="8">
+              <Card className="card-chart">
+                <CardHeader>
+                  <CardTitle tag="h5">NASDAQ: AAPL</CardTitle>
+                  <p className="card-category">Line Chart with Points</p>
+                </CardHeader>
+                <CardBody>
+                  <Line
+                    data={dashboardNASDAQChart.data}
+                    options={dashboardNASDAQChart.options}
+                    width={400}
+                    height={100}
+                  />
+                </CardBody>
+                <CardFooter>
+                  <div className="chart-legend">
+                    <i className="fa fa-circle text-info" /> Tesla Model S{" "}
+                    <i className="fa fa-circle text-warning" /> BMW 5 Series
+                  </div>
+                  <hr />
+                  <div className="card-stats">
+                    <i className="fa fa-check" /> Data information certified
+                  </div>
+                </CardFooter>
+              </Card>
+            </Col>
+          </Row>
+        </div>
+      </>
+    );
+  }
+}
+
+export default Dashboard;
